d4e5b4e3facd012f6bdb11b625f62a91b8fc468f
pwangy <blipsandclicks@gmail.com>
2024-01-26 16:38:39 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+let currentUser = 'Peggy'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+console.log(excitedWelcomeMessage)
+const shortGreeting = `Welcome, ${currentUser[0]}!`
