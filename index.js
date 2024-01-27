3af243891e724f116a0258ddb68618b9406341d7
pwangy <blipsandclicks@gmail.com>
2024-01-26 16:40:15 -0800

    clean up console.log

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 let currentUser = 'Peggy'
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
-console.log(excitedWelcomeMessage)
 const shortGreeting = `Welcome, ${currentUser[0]}!`
